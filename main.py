c6e742a1af030095cf7243a7598c6c7ea80e9903
Oscar <110410670+Osc44r@users.noreply.github.com>
2022-09-07 15:29:47 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+def pureConvert(refs,recs,scraps):
+    scrap = 0.11
+    reclaimed = 0.33
+    refined = 1
+
+    if scraps > 2:
+        if scraps%3 == 0:
+            recs = recs+scraps/3
+            scraps = 0
+
+        if scraps%3 == 1:
+            recs = recs+(scraps-1)/3
+            scraps = 1
+        if scraps%3 == 2:
+            recs = recs+(scraps-2)/3
+            scraps = 2
+
+    if recs > 2:
+        if recs%3 == 0:
+            refs = refs+recs/3
+            recs = 0
+        if recs%3 == 1:
+            refs = refs+(recs-1)/3
+            recs = 1
+        if recs%3 == 2:
+            refs = refs+(recs-2)/3
+            recs = 2
+
+    global pure
+    pure = round(refs*refined+recs*reclaimed+scraps*scrap,2)
+    print(pure)
+
+pureConvert(11,66,22) #example
+
+    
